2abb80d36cf5ea1667459adfd36989ddfececbd1
doxterpepper <dockconnellyoneal@gmail.com>
2017-04-13 17:18:20 -0400

    No need for the buffer variable

--- main.c
+++ main.c
@@ -12,15 +12,15 @@ int main(int argc, char** argv)
   while (1)
   {
     char input[512];
-    char buffer[512];
     char* nl;
     char** tokens;
     unsigned int num_tok;
+
+    nl = malloc(sizeof(char));
   
     write(STDOUT_FILENO, ps1, strlen(ps1));
-    read(STDIN_FILENO, buffer, 512);
+    read(STDIN_FILENO, input, 512);
 
-    strcpy(input, buffer);
     // Strip new line from input
     if ((nl=strchr(input, '\n')) != NULL)
       *nl = '\0';
